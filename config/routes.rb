5e41e74e75ef0443a4066c587d3cc55c3a1e95ec
冯璐 <fenglu@fenglus-MacBook-Pro.local>
2017-04-11 14:36:27 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,10 @@ end
 
 namespace :account do
   resources :groups
+  resources :posts
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'groups#index'
-  end 
+  end
