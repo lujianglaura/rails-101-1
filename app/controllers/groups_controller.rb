84ebaa5e5fd214814fe8e332872fae7944039245
冯璐 <fenglu@fenglus-MacBook-Pro.local>
2017-04-11 13:44:17 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
-
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy, :join, :quit]
+  
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :find_group_and_check_permission, only: [:edit, :update, :destroy, :join, :quit]
 
   def index
     @groups = Group.all
